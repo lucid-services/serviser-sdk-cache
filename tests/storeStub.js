787a3f7538daa24fad8f55e0567b3e37ac13a9a3
fogine <7884288+fogine@users.noreply.github.com>
2017-10-10 15:53:56 +0200

    fix tests

--- tests/storeStub.js
+++ tests/storeStub.js
@@ -1,6 +1,6 @@
-var Promise = require('bluebird');
-var sinon = require('sinon');
-var CacheStoreInterface = require('bi-cache-store-interface');
+const Promise = require('bluebird');
+const sinon = require('sinon');
+const CacheStoreInterface = require('bi-cache-store-interface');
 
 module.exports = StoreStub;
 
@@ -15,6 +15,49 @@ function StoreStub() {
 StoreStub.prototype = Object.create(CacheStoreInterface.prototype);
 StoreStub.prototype.constructor = StoreStub;
 
+/**
+ * @public
+ * @param {String} key
+ * @param {Function} cb - callback
+ * @return {undefined}
+ */
+StoreStub.prototype.get = function(key, cb) {
+    if (this.store.hasOwnProperty(key)) {
+        return cb(null, this.store[key]);
+    }
+    return cb(new CacheStoreInterface.NotFoundError(key));
+};
+
+/**
+ * @public
+ * @param {String}  key
+ * @param {mixed}   data
+ * @param {Integer} ttl - in seconds
+ * @param {Function} cb - callback
+ *
+ * @throws {Error} - async
+ * @return {undefined}
+ */
+StoreStub.prototype.set = function(key, data, ttl, cb) {
+    this.store[key] = data;
+
+    if (typeof ttl === 'number' && ttl !== 0) {
+
+        setTimeout(function(self, key) {
+            delete self.store[key];
+        }, ttl*1000, this, key);
+    }
+
+    return cb();
+};
+
+/**
+ * @public
+ * @return {Promise}
+ */
+StoreStub.prototype.inspectIntegrity = function() {
+    return Promise.resolve(true);
+};
 
 /**
  * @public
@@ -25,7 +68,6 @@ StoreStub.prototype.constructor = StoreStub;
  * @return {Promise}
  */
 StoreStub.prototype.fetch = function(key) {
-    var self = this;
     if (this.store.hasOwnProperty(key)) {
         return Promise.resolve(this.store[key]);
     } else {
